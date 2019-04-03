c69cf587559602f22bac316b47aeb4be7c8c1d9c
desmond <desiba_1@yahoo.com>
2019-04-03 12:53:55 +0100

    Revert "feature(user create account api):here user can create account Finishe…"

--- api/routes/api/v1/user.route.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import UserController  from '../../../controllers/user.controller';
-
-const router = express.Router();
-router.post('/', UserController.createAccount);
-
-export default router;
