7ff4707cac4d607fdde95d09059e21a66540559f
desiba <desiba_1@yahoo.com>
2019-04-09 04:14:20 +0100

    feature(ft user create account): user can create multiple accounts #165204549

--- api/routes/api/v1/user.route.js
+++ api/routes/api/v1/user.route.js
@@ -3,7 +3,7 @@ import UserController  from '../../../controllers/user.controller';
 
 const router = express.Router();
 
-router.post('/', UserController.createAccount);
+router.post('/account/', UserController.createAccount);
 router.get('/:id', UserController.fetchAccountById);
 router.get('/transaction/:id', UserController.getTransactionHistory);
 router.post('/transaction/:accountnumber/debit', UserController.debitAccount);
